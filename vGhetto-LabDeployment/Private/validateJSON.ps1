84c1eb4e5ad727d9a95be7eaf205b536254d8ad5
pezhore@gmail.com <=>
2017-08-26 16:04:17 -0500

    first attempt at json validation

--- vGhetto-LabDeployment/Private/validateJSON.ps1
+++ vGhetto-LabDeployment/Private/validateJSON.ps1
@@ -9,7 +9,7 @@ function validateJson
     BEGIN {}
     PROCESS
     {
-        # Try to import/convert
+        # Try to import/convert from JSON
         try
         {
             $config = Get-Content $json | ConvertFrom-Json -ErrorAction Stop
@@ -21,9 +21,25 @@ function validateJson
             Return $config            
         }
 
-        
-        
-
+        # Check if the binaries are valid
+        foreach ($binary in $($config.Binaries | Get-Member -MemberType NoteProperty).Name )
+        {
+            Remove-Variable pathType -ErrorAction SilentlyContinue
+            if ($binary -match "path")
+            {
+                $pathType = "Container"
+            }
+            else
+            {
+                $pathType = "Leaf"
+            }
+            if (! (Test-path -Type $pathType $Config.Binaries.$binary))
+            {
+                Write-Debug "Binary $binary not valid $pathType, debug?"
+                $config | Add-Member -MemberType NoteProperty -Name Error -Value "Binary $binary not valid $pathType"
+                return $config
+            }
+        }
     }
     END
     {
